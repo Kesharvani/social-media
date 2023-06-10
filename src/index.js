16bfc5e396646a868d3eabd57ff346d0697cb66f
Kesharvani <shubhamkesharvani888@gmail.com>
2023-06-10 15:32:07 +0530

    feat:created router component

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { makeServer } from "./server";
-
+import { BrowserRouter } from "react-router-dom";
+import { Routers } from "./Routers";
 // Call make Server
 makeServer();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <Routers />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
