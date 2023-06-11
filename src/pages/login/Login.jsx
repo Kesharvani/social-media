cd578394baf6b97cbfba6ab22cc10a6ad881f1a5
Kesharvani <shubhamkesharvani888@gmail.com>
2023-06-11 19:29:57 +0530

    refactor:changed folder name in small case

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,5 @@
 import "./Login.css";
-import { LoginContainer } from "./Component/LoginContainer";
+import { LoginContainer } from "./component/LoginContainer";
 export const Login = () => {
   return (
     <>
