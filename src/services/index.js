598a6aad6c7b5fdfd0891ed8dbc611905f60cbc8
Kesharvani <shubhamkesharvani888@gmail.com>
2023-06-12 21:46:45 +0530

    fix:exported post services

--- src/services/index.js
+++ src/services/index.js
@@ -3,4 +3,5 @@ export * from "./followUnfollow/followUnfollowServices";
 export * from "./likeDislike/likeDislikeServices";
 export * from "./login/loginServices";
 export * from "./signup/signupServices";
+export * from "./post/postServices";
 export * from "./user/userServices";
