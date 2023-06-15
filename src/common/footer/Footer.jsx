44e42ac0b65efe87ba91194d1f37118aa6255046
Kesharvani <shubhamkesharvani888@gmail.com>
2023-06-15 22:54:15 +0530

    feat:added react icons in footer

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -1,20 +1,38 @@
 import { Link } from "react-router-dom";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { BsFacebook } from "react-icons/bs";
 
 import Hi5 from "../../assets/Hi5.jpeg";
+
 export const Footer = () => {
   return (
-    <div className="text-center">
+    <div className="flex flex-col gap-4  items-center">
       <div className="flex justify-center gap-4">
-        <img src={Hi5} alt="logo" width="70px" height="50px" />
+        <Link to="/">
+          <img src={Hi5} alt="logo" width="70px" height="50px" />
+        </Link>
       </div>
       <div className="flex gap-4 justify-center">
-        <span>github</span>
-        <span>facebook</span>
-        <span>twitter</span>
+        <span>
+          <Link to="https://google.com">
+            <BsLinkedin size={24} />
+          </Link>
+        </span>
+        <span>
+          <Link to="/">
+            <BsGithub size={24} />
+          </Link>
+        </span>
+        <span>
+          <Link to="/">
+            <BsFacebook size={24} />
+          </Link>
+        </span>
       </div>
       <div>
         Copyright
-        <span className="before:content-['\24B8']">Shubham Kesharvani</span>
+        {" "}<span className="before:content-['\24B8']"> Shubham Kesharvani</span>
       </div>
     </div>
   );
