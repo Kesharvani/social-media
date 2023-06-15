34a997b87afb874aef77570e9d87901a1ea02b3c
Kesharvani <shubhamkesharvani888@gmail.com>
2023-06-16 01:17:51 +0530

    style: added for header

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -3,14 +3,14 @@ import Hi5 from "../../assets/Hi5.jpeg";
 export const Header = () => {
   return (
     <>
-      <div className="flex justify-around">
+      <div className="flex px-12 py-4 justify-between items-center">
         <div>
           <img src={Hi5} alt="logo" width="70px" height="50px" />
         </div>
         <div>
-          <input type="text" name="search" placeholder="Search By User"/>
+          <input type="text" name="search" placeholder="Search By User" className="border-white border py-2 pr-6 pl-2"/>
         </div>
-        <div>userProfilePicture</div>
+        <div><img src={Hi5} alt="userProfile" className="border rounded-full" width="70px" height="50px"/></div>
       </div>
     </>
   );
