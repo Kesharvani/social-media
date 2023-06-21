132ffb1c51d79e62ae19767546023bf4bc917a27
Kesharvani <shubhamkesharvani888@gmail.com>
2023-06-21 23:34:53 +0530

    feat:added side bar for navigation

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import { Header } from "../../common/index";
 import {Footer} from "../../common/index"
+import {HomeContainer} from "./component/HomeContainer"
 export const Home = () => {
   return (
     <>
       <Header />
       <hr />
+      <HomeContainer/>
       <hr />
       <Footer/>
     </>
---
+++ src/pages/home/component/HomeContainer.jsx
@@ -0,0 +1,38 @@
+import { AiTwotoneHome } from "react-icons/ai";
+import { MdExplore } from "react-icons/md";
+import { BsBookmarkStar } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
+
+
+
+import {useState} from 'react'
+export const HomeContainer=()=>{
+    const [selectedTab,setSelectedTab]=useState('Home')
+    return (
+        <div className="flex">
+            <div className="flex flex-col flex-1 flex-wrap border-r-[1px]">
+                <button className={`flex  gap-3 items-center px-[2rem] py-[0.8rem] ${selectedTab==="Home"? "bg-slate-600":''}`} onClick={()=>setSelectedTab("Home")}>
+                    <AiTwotoneHome size={24} className={`${selectedTab==="Home"? "bg-slate-600":''}`}/>
+                    <p className={`text-xl ${selectedTab==="Home"? "bg-slate-600":''}`}>Home</p>
+                </button>
+                <button  className={`flex  gap-3 items-center px-[2rem] py-[0.8rem] ${selectedTab==="Explore"? "bg-slate-600":''}`} onClick={()=>setSelectedTab("Explore")}>
+                    <MdExplore size={24} className={`${selectedTab==="Explore"? "bg-slate-600":''}`}/>
+                    <p className={`text-xl ${selectedTab==="Explore"? "bg-slate-600":''}`}>Explore</p>
+                </button>
+                <button  className={`flex  gap-3 items-center px-[2rem] py-[0.8rem] ${selectedTab==="Bookmarks"? "bg-slate-600":''}`} onClick={()=>setSelectedTab("Bookmarks")} >
+                    <BsBookmarkStar size={24} className={`${selectedTab==="Bookmarks"? "bg-slate-600":''}`}/>
+                    <p className={`text-xl ${selectedTab==="Bookmarks"? "bg-slate-600":''}`}>Bookmarks</p>
+                </button>
+                <button  className={`flex  gap-3 items-center px-[2rem] py-[0.8rem] ${selectedTab==="Liked"? "bg-slate-600":''}`} onClick={()=>setSelectedTab("Liked")}>
+                    <AiOutlineHeart size={24} className={`${selectedTab==="Liked"? "bg-slate-600":''}`}/>
+                    <p className={`text-xl ${selectedTab==="Liked"? "bg-slate-600":''}`}>Liked</p>
+                </button>
+                <button  className={`text-xl border text-white px-[2rem] py-[0.8rem] hover:bg-slate-400 mt-[0.5rem] ${selectedTab==="Post"? "bg-slate-600":''}`} onClick={()=>setSelectedTab("Post")}>
+                    Post
+                </button>
+            </div>
+            <div className="flex-1 grow-[7]">div 2</div>
+            <div className="flex-1 flex-wrap">div 3</div>
+        </div>
+    )
+}
