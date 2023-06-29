abd55b46164950928676ca3f12d909e3c3c28811
Kesharvani <shubhamkesharvani888@gmail.com>
2023-06-30 00:37:40 +0530

    fix:home page post display

--- src/pages/home/component/HomeContainer.jsx
+++ src/pages/home/component/HomeContainer.jsx
@@ -4,13 +4,19 @@ import { UserSuggestionTile } from "../../../common/userSuggestionTile/UserSugge
 import { usePost } from "../../../context/PostContext";
 
 export const HomeContainer = () => {
-  const { state } = usePost();
+  const { state, user } = usePost();
 
+  const filteredPostsForHome = state.posts.filter(
+    (post) =>
+      user.following.some(
+        (userFollowing) => userFollowing.username === post.username
+      ) || user.username === post.username
+  );
   return (
     <div className="flex">
       <Sidebar />
       <section className="flex-1 grow-[7]">
-        <PostUserTile posts={state?.posts} />
+        <PostUserTile posts={filteredPostsForHome} />
       </section>
       <section className="flex-1 flex-wrap grow-[1.8]">
         <UserSuggestionTile allUser={state.allUser} />
