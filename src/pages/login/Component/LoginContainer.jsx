4c52daf8d777dd6ce818aa674c7b9aaa69fefaad
Kesharvani <shubhamkesharvani888@gmail.com>
2023-07-02 11:30:49 +0530

    style:for login and signup

--- src/pages/login/Component/LoginContainer.jsx
+++ src/pages/login/Component/LoginContainer.jsx
@@ -18,34 +18,40 @@ export const LoginContainer = () => {
   };
 
   return (
-    <>
-      <form onSubmit={clickHandler}>
+    <div className="flex justify-center items-center h-screen">
+      <form
+        onSubmit={clickHandler}
+        className="flex flex-col gap-5 w-[28rem] mb-[3rem] p-[2rem] shadow-white shadow-md"
+      >
+        <h2 className="text-[1.87rem]">Login</h2>
         <label htmlFor="user_id">
-          User Name
+          User name:
           <input
             type="text"
             id="user_id"
             name="username"
             placeholder="Enter User Name"
+            className="px-[0.5rem] py-[0.7rem] w-[24rem] mt-[0.5rem] border border-white rounded-md"
             value={inputData.username}
             onChange={inputHandler}
           />
         </label>
 
         <label htmlFor="password">
-          Password
+          Password:
           <input
             type="password"
             id="password"
             name="password"
             placeholder="Enter password"
+            className="px-[0.5rem] py-[0.7rem] w-[24rem] mt-[0.5rem] border-white rounded-md"
             value={inputData.password}
             onChange={inputHandler}
           />
         </label>
 
-        <input type="submit" />
+        <input type="submit" className="border border-white rounded-md p-[0.5rem] w-[24rem] bg-[#a82723] hover:bg-[#b92b27] cursor-pointer" />
       </form>
-    </>
+    </div>
   );
 };
--- src/pages/register/component/RegisterContainer.jsx
+++ src/pages/register/component/RegisterContainer.jsx
@@ -34,65 +34,74 @@ export const RegisterContainer = () => {
     signupHandler("guest", "guest", "Guest", "guest");
   };
   return (
-    <>
-      <form onSubmit={submitHandler}>
-        <div>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <form onSubmit={submitHandler} className="flex flex-col gap-5 w-[30rem] mb-[3rem] p-[2rem] shadow-white shadow-md">
+        <h3 className="text-[1.87rem]">Sign up</h3>
+        <div className="flex flex-col">
           <label htmlFor="gender">Gender(Optional)</label>
-          <select name="gender" id="gender">
+          <select name="gender" id="gender" className="px-[0.5rem] py-[0.7rem] w-[24rem] mt-[0.5rem] border border-white rounded-md">
             <option value="">Choose Gender</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
           </select>
         </div>
-        <div>
+        <div className="flex flex-col">
           <label htmlFor="first_name"> First Name</label>
           <input
             type="text"
             id="first_name"
             name="firstName"
+            className="px-[0.5rem] py-[0.7rem] w-[24rem] mt-[0.5rem] border border-white rounded-md"
             value={inputData.firstName}
             onChange={onChangeHandler}
+            required
           />
         </div>
-        <div>
+        <div className="flex flex-col">
           <label htmlFor="last_name"> Last Name(Optional)</label>
-          <input type="text" id="last_name" />
+          <input type="text" id="last_name" className="px-[0.5rem] py-[0.7rem] w-[24rem] mt-[0.5rem] border border-white rounded-md"/>
         </div>
-        <div>
+        <div className="flex flex-col">
           <label htmlFor="date_of_birth"> Date Of Birth</label>
           <input
             type="date"
             id="date_of_birth"
             name="dateOfBirth"
+            className="px-[0.5rem] py-[0.7rem] w-[24rem] mt-[0.5rem] border border-white rounded-md"
             value={inputData.dateOfBirth}
             onChange={onChangeHandler}
+            required
           />
         </div>
-        <div>
+        <div className="flex flex-col">
           <label htmlFor="user_name"> User Name</label>
           <input
             type="text"
             id="user_name"
             name="userName"
+            className="px-[0.5rem] py-[0.7rem] w-[24rem] mt-[0.5rem] border border-white rounded-md"
             value={inputData.userName}
             onChange={onChangeHandler}
+            required
           />
         </div>
-        <div>
+        <div className="flex flex-col">
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
             name="password"
+            className="px-[0.5rem] py-[0.7rem] w-[24rem] mt-[0.5rem] border border-white rounded-md"
             value={inputData.password}
             onChange={onChangeHandler}
+            required
           />
         </div>
         <div>
-          <input type="submit" />
+          <input type="submit" className="border border-white rounded-md p-[0.5rem] w-[24rem] bg-[#a82723] hover:bg-[#b92b27] cursor-pointer"/>
         </div>
       </form>
-      <button onClick={guestHandler}>Login As Guest</button>
-    </>
+      <button onClick={guestHandler} className="border border-white rounded-md p-[0.5rem] w-[24rem] bg-[#a82723] hover:bg-[#b92b27] cursor-pointer">Login As Guest</button>
+    </div>
   );
 };
